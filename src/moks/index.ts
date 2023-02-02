f3b56aaa8b41fa7ad5c47f4b3720166b0822e1dc
terrydaviz <iltihomsorok@gmail.com>
2023-02-02 16:17:01 +0300

    initial commit

---
+++ src/moks/index.ts
@@ -0,0 +1,272 @@
+export const users = [
+  {
+    "_id": "63d940c6da4c5f7cef62f49f",
+    "index": 0,
+    "guid": "9ba13167-a0e3-4fc6-ae50-7aef65594ac6",
+    "isActive": false,
+    "balance": "$3,331.22",
+    "picture": "http://placehold.it/32x32",
+    "age": 40,
+    "eyeColor": "brown",
+    "name": "Savage Estes",
+    "gender": "male",
+    "company": "ACCUFARM",
+    "email": "savageestes@accufarm.com",
+    "phone": "+1 (804) 420-2778",
+    "address": "319 Anchorage Place, Idamay, Mississippi, 3666",
+    "about": "Anim ex non reprehenderit irure et irure voluptate dolor proident ullamco eiusmod et. Elit cillum id exercitation esse incididunt proident laborum pariatur quis proident commodo. Nulla cupidatat consequat nisi culpa esse cillum excepteur commodo consequat aute aliqua irure incididunt magna. Enim ullamco ipsum qui ipsum non dolor dolor commodo ullamco. Est adipisicing aute adipisicing cupidatat minim officia tempor sunt veniam aliqua in aliquip.\r\n",
+    "registered": "2021-08-21T03:58:18 -03:00",
+    "latitude": 76.317415,
+    "longitude": -61.50572,
+    "tags": [
+      "cupidatat",
+      "fugiat",
+      "in",
+      "nisi",
+      "ad",
+      "proident",
+      "excepteur"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Benton Chandler"
+      },
+      {
+        "id": 1,
+        "name": "Webb Skinner"
+      },
+      {
+        "id": 2,
+        "name": "Burgess Walls"
+      }
+    ],
+    "greeting": "Hello, Savage Estes! You have 9 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "63d940c6faee98c170febd37",
+    "index": 1,
+    "guid": "71c6a444-6e40-44bb-8361-a035905b6ee1",
+    "isActive": false,
+    "balance": "$2,252.87",
+    "picture": "http://placehold.it/32x32",
+    "age": 26,
+    "eyeColor": "blue",
+    "name": "Margie Maynard",
+    "gender": "female",
+    "company": "CENTICE",
+    "email": "margiemaynard@centice.com",
+    "phone": "+1 (883) 533-2438",
+    "address": "894 Laurel Avenue, Richford, Delaware, 8856",
+    "about": "Dolor occaecat culpa incididunt amet cillum do ex. Non exercitation elit irure laborum occaecat culpa. Enim et adipisicing occaecat excepteur reprehenderit magna. Aliqua reprehenderit elit aute cupidatat. Sint sint esse tempor et laboris proident.\r\n",
+    "registered": "2016-08-04T11:14:53 -03:00",
+    "latitude": -4.859141,
+    "longitude": -16.079456,
+    "tags": [
+      "culpa",
+      "elit",
+      "ut",
+      "laborum",
+      "sunt",
+      "fugiat",
+      "dolor"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Marcie Henry"
+      },
+      {
+        "id": 1,
+        "name": "Leanne Dillard"
+      },
+      {
+        "id": 2,
+        "name": "Doyle Shepherd"
+      }
+    ],
+    "greeting": "Hello, Margie Maynard! You have 9 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "63d940c6a3bdaa09b5d4ed49",
+    "index": 2,
+    "guid": "61a202c6-51af-48ea-af76-d847e1a3b019",
+    "isActive": true,
+    "balance": "$2,816.71",
+    "picture": "http://placehold.it/32x32",
+    "age": 34,
+    "eyeColor": "green",
+    "name": "Cochran Berg",
+    "gender": "male",
+    "company": "GEEKFARM",
+    "email": "cochranberg@geekfarm.com",
+    "phone": "+1 (970) 520-3813",
+    "address": "290 Celeste Court, Moraida, Missouri, 6450",
+    "about": "Do aute ipsum eu commodo fugiat nisi irure sint nisi elit. Mollit fugiat anim aute reprehenderit dolore mollit pariatur enim fugiat voluptate reprehenderit aliqua cupidatat. Quis magna eiusmod est ullamco ullamco velit magna elit dolor laboris consectetur.\r\n",
+    "registered": "2019-01-10T01:37:23 -03:00",
+    "latitude": -53.456253,
+    "longitude": -37.129824,
+    "tags": [
+      "amet",
+      "ad",
+      "sint",
+      "sint",
+      "occaecat",
+      "aliqua",
+      "sunt"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Misty Ashley"
+      },
+      {
+        "id": 1,
+        "name": "Santana Lyons"
+      },
+      {
+        "id": 2,
+        "name": "Mcfadden Short"
+      }
+    ],
+    "greeting": "Hello, Cochran Berg! You have 10 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "63d940c6f26dffe1eb7a7daa",
+    "index": 3,
+    "guid": "ee36e1b1-135e-49bf-adc9-a2be495c9fb7",
+    "isActive": true,
+    "balance": "$3,587.36",
+    "picture": "http://placehold.it/32x32",
+    "age": 37,
+    "eyeColor": "green",
+    "name": "Cassie Bryan",
+    "gender": "female",
+    "company": "BICOL",
+    "email": "cassiebryan@bicol.com",
+    "phone": "+1 (883) 403-3628",
+    "address": "971 Lott Avenue, Harleigh, South Dakota, 5303",
+    "about": "Lorem incididunt voluptate est in ea deserunt consectetur ut officia culpa Lorem pariatur id. Ad ipsum deserunt est mollit ea incididunt exercitation velit irure. Magna ad nostrud veniam qui laboris commodo cupidatat.\r\n",
+    "registered": "2022-02-11T11:06:49 -03:00",
+    "latitude": -50.858858,
+    "longitude": 32.005302,
+    "tags": [
+      "sunt",
+      "veniam",
+      "officia",
+      "sit",
+      "cillum",
+      "qui",
+      "in"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Blanca Weeks"
+      },
+      {
+        "id": 1,
+        "name": "Malinda Mccarty"
+      },
+      {
+        "id": 2,
+        "name": "Case Cooley"
+      }
+    ],
+    "greeting": "Hello, Cassie Bryan! You have 5 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "63d940c69d58d5f8f44e6455",
+    "index": 4,
+    "guid": "b6c76868-04ae-4c05-9cdb-3a2658078910",
+    "isActive": true,
+    "balance": "$3,280.19",
+    "picture": "http://placehold.it/32x32",
+    "age": 32,
+    "eyeColor": "green",
+    "name": "Hall Bell",
+    "gender": "male",
+    "company": "ZENSURE",
+    "email": "hallbell@zensure.com",
+    "phone": "+1 (924) 510-3584",
+    "address": "319 Dakota Place, Advance, Marshall Islands, 6344",
+    "about": "Eiusmod laborum velit sunt in voluptate voluptate anim exercitation Lorem adipisicing. Ad dolor qui nisi qui culpa. Magna nisi sint incididunt eiusmod. Nisi adipisicing non laborum irure elit deserunt dolor aliquip est irure.\r\n",
+    "registered": "2016-01-19T05:50:44 -03:00",
+    "latitude": 57.631266,
+    "longitude": -121.368489,
+    "tags": [
+      "culpa",
+      "dolore",
+      "irure",
+      "laboris",
+      "est",
+      "cupidatat",
+      "excepteur"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Beard Nunez"
+      },
+      {
+        "id": 1,
+        "name": "Beth Simpson"
+      },
+      {
+        "id": 2,
+        "name": "Conrad Johnston"
+      }
+    ],
+    "greeting": "Hello, Hall Bell! You have 10 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "63d940c6dda707d92d6ce76e",
+    "index": 5,
+    "guid": "da64d203-da10-4c82-817b-a1d29841bae4",
+    "isActive": false,
+    "balance": "$3,858.44",
+    "picture": "http://placehold.it/32x32",
+    "age": 36,
+    "eyeColor": "brown",
+    "name": "Yesenia Christensen",
+    "gender": "female",
+    "company": "TWIIST",
+    "email": "yeseniachristensen@twiist.com",
+    "phone": "+1 (805) 574-3642",
+    "address": "165 Bridge Street, Kennedyville, Montana, 6343",
+    "about": "Voluptate irure sint ut quis. Commodo exercitation sit tempor sint adipisicing est ipsum dolore. Proident culpa quis magna esse fugiat irure veniam. Id ut sint consectetur excepteur anim enim ea ullamco non eu occaecat. Proident cupidatat nulla pariatur dolor duis labore.\r\n",
+    "registered": "2018-06-02T01:05:03 -03:00",
+    "latitude": -21.735352,
+    "longitude": -40.258274,
+    "tags": [
+      "veniam",
+      "enim",
+      "dolore",
+      "pariatur",
+      "sit",
+      "veniam",
+      "ipsum"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Rose Salazar"
+      },
+      {
+        "id": 1,
+        "name": "Edwards Stewart"
+      },
+      {
+        "id": 2,
+        "name": "Leah Wong"
+      }
+    ],
+    "greeting": "Hello, Yesenia Christensen! You have 9 unread messages.",
+    "favoriteFruit": "apple"
+  }
+]
